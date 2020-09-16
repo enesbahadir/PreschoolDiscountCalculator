afa6ddb3774dac05ad027c9908b677fd747a16f1
Enes Yıldırım <enesbahadiryildirim>
2020-09-16 17:41:01 +0300

    PreschoolNames enum'u kaldırıldı, preschool nesnelerinin adları eklendi, OrganizationNames.NONE olan attribute'lar null ile değiştirildi

--- src/com/company/DefaultDiscountList.java
+++ src/com/company/DefaultDiscountList.java
@@ -12,28 +12,28 @@ public enum DefaultDiscountList {
                                     add(TypeOfUser.IHVAN);
                                     add(TypeOfUser.STANDART);
                                 }},
-                                OrganizationNames.NONE,
+                                null,
                                 new ArrayList<>() {{
-                                    add(PreschoolNames.MADENLER_LALEBAHCESI);
-                                    add((PreschoolNames.YUNUS_EMRE_LALEBAHCESI));
+                                    add(PreschoolList.YUNUS_EMRE_LALEBAHCESI.name());
+                                    add((PreschoolList.YUNUS_EMRE_LALEBAHCESI).name());
                                 }} )),
     PERSONNEL_DISCOUNT(new Discount("PERSONNEL_DISCOUNT",50L,DiscountType.PERCENTAGE,
                                     new ArrayList<>() {{
                                         add(TypeOfUser.PERSONEL);
                                     }},
-                                    OrganizationNames.NONE,
+                                    null,
                                     new ArrayList<>() {{
-                                        add(PreschoolNames.MADENLER_LALEBAHCESI);
-                                        add((PreschoolNames.YUNUS_EMRE_LALEBAHCESI));
+                                        add(PreschoolList.MADENLER_LALEBAHCESI.name());
+                                        add((PreschoolList.YUNUS_EMRE_LALEBAHCESI).name());
                                     }} )),
     IHVAN_DISCOUNT(new Discount("IHVAN_DISCOUNT",5L,DiscountType.PERCENTAGE,
                                 new ArrayList<>() {{
                                     add(TypeOfUser.IHVAN);
                                 }},
-                                OrganizationNames.NONE,
+                                null,
                                 new ArrayList<>() {{
-                                    add(PreschoolNames.MADENLER_LALEBAHCESI);
-                                    add((PreschoolNames.YUNUS_EMRE_LALEBAHCESI));
+                                    add(PreschoolList.MADENLER_LALEBAHCESI.name());
+                                    add((PreschoolList.YUNUS_EMRE_LALEBAHCESI.name()));
                                 }} )),
     SAGLIKSEN_DISCOUNT(new Discount("SAGLIKSEN_DISCOUNT",10L,DiscountType.PERCENTAGE,
                                     new ArrayList<>() {{
@@ -42,7 +42,7 @@ public enum DefaultDiscountList {
                                     }},
                                     OrganizationNames.SAGLIKSEN,
                                     new ArrayList<>() {{
-                                        add(PreschoolNames.MADENLER_LALEBAHCESI);
+                                        add(PreschoolList.MADENLER_LALEBAHCESI.name());
                                     }} )),
     ANADOLU_DISCOUNT(new Discount("ANADOLU_DISCOUNT",100L,DiscountType.AMOUNT,
                                     new ArrayList<>() {{
@@ -51,7 +51,7 @@ public enum DefaultDiscountList {
                                     }},
                                     OrganizationNames.ANADOLU,
                                     new ArrayList<>() {{
-                                        add(PreschoolNames.YUNUS_EMRE_LALEBAHCESI);
+                                        add(PreschoolList.YUNUS_EMRE_LALEBAHCESI.name());
                                     }} )),;
 
     DefaultDiscountList(Discount discount) {
